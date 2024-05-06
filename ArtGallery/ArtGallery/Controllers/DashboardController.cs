34075141912528a3ffde1284b455b663dc279b99
Phuong <phuongphuong262204@gmail.com>
2024-05-06 15:32:49 +0700

    update Dashboard

--- ArtGallery/ArtGallery/Controllers/DashboardController.cs
+++ ArtGallery/ArtGallery/Controllers/DashboardController.cs
@@ -1,8 +1,12 @@
-﻿using ArtGallery.Entities;
+﻿using ArtGallery.DTOs;
+using ArtGallery.Entities;
+using ArtGallery.Models.GeneralService;
 using ArtGallery.Service.Artists;
 using ArtGallery.Service.IMG;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Security;
 
 namespace ArtGallery.Controllers
 {
@@ -11,11 +15,11 @@ namespace ArtGallery.Controllers
     public class DashboardController : ControllerBase
     {
         private readonly ArtGalleryApiContext _context;
-      
+
         public DashboardController(ArtGalleryApiContext context)
         {
             _context = context;
-           
+
         }
 
         // Endpoint để lấy tổng số lượng offer
@@ -25,10 +29,10 @@ namespace ArtGallery.Controllers
             try
             {
                 // Sử dụng LINQ để đếm số lượng offer trong cơ sở dữ liệu
-                var totalCount = await _context.Offer.Where(m => m.DeletedAt == null).CountAsync();
+                var totalOffer = await _context.Offer.Where(m => m.DeletedAt == null).CountAsync();
 
                 // Trả về số lượng offer trong response
-                return Ok(totalCount);
+                return Ok(totalOffer);
             }
             catch (Exception ex)
             {
@@ -38,33 +42,28 @@ namespace ArtGallery.Controllers
         }
 
         [HttpGet("total-count-user")]
-        public async Task<IActionResult> GetTotalUserCount()
-        {
-            try
-            {
-                // Sử dụng LINQ để đếm số lượng user trong cơ sở dữ liệu
-                var totalCount = await _context.Users.Where(m => m.DeletedAt == null).CountAsync();
 
-                // Trả về số lượng user trong response
-                return Ok(totalCount);
-            }
-            catch (Exception ex)
+        public async Task<IActionResult> GetUserCount()
+        {
+            var userCount = new
             {
-                // Xử lý nếu có lỗi xảy ra
-                return StatusCode(500, "Internal server error");
-            }
+                TotalUser = await _context.Users.Where(u => u.Role.Equals("User")).CountAsync(),
+                TotalArtist = await _context.Users.Where(u => u.Role.Equals("Artist")).CountAsync(),
+            };
+
+            return Ok(userCount);
         }
 
-        [HttpGet("total-count-Artist")]
+        [HttpGet("total-count-artist")]
         public async Task<IActionResult> GetTotalArtistCount()
         {
             try
             {
                 // Sử dụng LINQ để đếm số lượng artist trong cơ sở dữ liệu
-                var totalCount = await _context.Artist.Where(m => m.DeletedAt == null).CountAsync();
+                var totalArtist = await _context.Artist.Where(m => m.DeletedAt == null).CountAsync();
 
                 // Trả về số lượng artist trong response
-                return Ok(totalCount);
+                return Ok(totalArtist);
             }
             catch (Exception ex)
             {
@@ -73,16 +72,16 @@ namespace ArtGallery.Controllers
             }
         }
 
-        [HttpGet("total-count")]
+        [HttpGet("total-count-artwork")]
         public async Task<IActionResult> GetTotalArtworkCount()
         {
             try
             {
                 // Sử dụng LINQ để đếm số lượng artwork trong cơ sở dữ liệu
-                var totalCount = await _context.ArtWork.Where(m => m.DeletedAt == null).CountAsync();
+                var totalArtwork = await _context.ArtWork.Where(m => m.DeletedAt == null).CountAsync();
 
                 // Trả về số lượng artwork trong response
-                return Ok(totalCount);
+                return Ok(totalArtwork);
             }
             catch (Exception ex)
             {
@@ -90,5 +89,172 @@ namespace ArtGallery.Controllers
                 return StatusCode(500, "Internal server error");
             }
         }
+
+
+        [HttpGet("revenue/yearly")]
+        public IActionResult GetYearlySales()
+        {
+            int currentYear = DateTime.UtcNow.Year;
+            int startYear = currentYear - 4;
+            List<int> allYears = Enumerable.Range(startYear, 5).ToList();
+            var yearlySales = allYears
+                .GroupJoin(_context.Offer,
+                    year => year,
+                    order => order.CreatedAt.Value.Year,
+                    (year, orders) => new
+                    {
+                        Year = year,
+                        TotalSales = orders.Sum(o => o.Total)
+                    })
+                .OrderBy(result => result.Year)
+                .ToList();
+
+            return Ok(yearlySales);
+        }
+
+        [HttpGet("revenue/monthly/{year}")]
+        public IActionResult GetMonthlySales(int year)
+        {
+            // Validate the input year
+            if (year <= 0)
+            {
+                return BadRequest("Invalid year parameter.");
+            }
+            DateTime startDate = new DateTime(year, 1, 1);
+            List<DateTime> allMonthsOfYear = Enumerable.Range(0, 12)
+                .Select(offset => startDate.AddMonths(offset))
+                .ToList();
+            var monthlySales = allMonthsOfYear
+                .GroupJoin(_context.Offer,
+                    date => new { Year = date.Year, Month = date.Month },
+                    order => new { Year = order.CreatedAt.Value.Year, Month = order.CreatedAt.Value.Month },
+                    (date, orders) => new
+                    {
+                        Year = date.Year,
+                        Month = date.Month,
+                        TotalSales = orders.Sum(o => o.Total)
+                    })
+                .OrderBy(result => result.Year)
+                .ThenBy(result => result.Month)
+                .Select(result => new
+                {
+                    Year = result.Year,
+                    Month = result.Month,
+                    TotalSales = result.TotalSales
+                })
+                .ToList();
+
+            return Ok(monthlySales);
+        }
+
+        [HttpGet("revenue/weekly")]
+        public IActionResult GetWeeklySales()
+        {
+            DateTime today = DateTime.UtcNow.Date;
+            List<DateTime> past7Days = Enumerable.Range(0, 7)
+                .Select(offset => today.AddDays(-offset))
+                .ToList();
+            var past7DaysSales = past7Days
+                .GroupJoin(_context.Offer,
+                    date => date.Date,
+                    order => order.CreatedAt.Value.Date,
+                    (date, orders) => new
+                    {
+                        Date = date,
+                        TotalSales = orders.Sum(o => o.Total)
+                    })
+                .Select(result => new
+                {
+                    Date = result.Date,
+                    TotalSales = result.TotalSales
+                })
+                .ToList();
+
+            return Ok(past7DaysSales);
+        }
+
+
+        [HttpGet("total-revenue")]
+        public async Task<IActionResult> GetTotalRevenue()
+        {
+            try
+            {
+                var offers = await _context.Offer
+                    .Where(o => o.Status == 1)
+                    .ToListAsync();
+                var totalRevenue = offers.Sum(o => o.Total);
+
+                return Ok(new { totalRevenue });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        // GET: api/Offer/TotalOfferToday
+        [HttpGet("total-offer-today")]
+        public async Task<IActionResult> GetTotalOfferToday()
+        {
+            try
+            {
+                DateTime today = DateTime.Today;
+                var totalOfferToday = await _context.Offer
+                    .Where(o => o.CreatedAt.HasValue && o.CreatedAt.Value.Date == today)
+                    .CountAsync();
+
+                return Ok(new { totalOfferToday });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpGet("list-offer-today")]
+        public async Task<IActionResult> GetListOfferToDay()
+        {
+            try
+            {
+                DateTime today = DateTime.Now.Date;
+                List<Offer> offers = await _context.Offer.Include(o => o.User).Include(o => o.OfferArtWorks)
+        .ThenInclude(oaw => oaw.ArtWork).Where(o => o.CreatedAt.Value.Date == today).OrderByDescending(p => p.CreatedAt).ToListAsync();
+                List<OfferDTO> result = new List<OfferDTO>();
+                foreach (var offer in offers)
+                {
+                    result.Add(new OfferDTO
+                    {
+                        OfferCode = offer.OfferCode,
+                        ArtWorkId = offer.ArtWorkId,
+                        UserId = offer.UserId,
+                        UserName = offer.User.Fullname,
+                        ToTal = offer.Total,
+                        Status = offer.Status,
+                        ArtWorkNames = offer.OfferArtWorks.Select(oaw => oaw.ArtWork.Name).ToList(),
+                        ArtWorkImages = offer.OfferArtWorks.Select(oaw => oaw.ArtWork.ArtWorkImage).ToList(),
+                        OfferPrice = offer.OfferPrice,
+                        createdAt = offer.CreatedAt,
+                        updatedAt = offer.UpdatedAt,
+                        deletedAt = offer.DeletedAt,
+                    });
+                }
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                var response = new GeneralService
+                {
+                    Success = false,
+                    StatusCode = 400,
+                    Message = ex.Message,
+                    Data = ""
+                };
+
+                return BadRequest(response);
+            }
+        }
+
     }
 }
