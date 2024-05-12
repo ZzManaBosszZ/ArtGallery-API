4e0ecf4cb5c43e0c9a2ea24201f3439c84226e58
hunganhnb2504 <hunganhnbng@gmail.com>
2024-05-12 13:42:27 +0700

    thêm Description artist

--- ArtGallery/ArtGallery/Entities/Artist.cs
+++ ArtGallery/ArtGallery/Entities/Artist.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; } = null!;
         public string Image {  get; set; }  
         public string? Biography { get; set; }
+
+        public string? Description { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
--- ArtGallery/ArtGallery/Models/Artist/EditArtistModel.cs
+++ ArtGallery/ArtGallery/Models/Artist/EditArtistModel.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Biography { get; set; }
+
+        public string? Description { get; set; }
         public IFormFile? ImagePath { get; set; }
     }
 }
