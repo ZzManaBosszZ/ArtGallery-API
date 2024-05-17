1ad6c708f8e83fa5235b5bc510a7cc7debaa2e65
hunganhnb2504 <hunganhnbng@gmail.com>
2024-05-17 11:30:19 +0700

    request artist

--- ArtGallery/ArtGallery/DTOs/FavoriteDTO.cs
+++ ArtGallery/ArtGallery/DTOs/FavoriteDTO.cs
@@ -6,6 +6,10 @@
         public string ArtWorkName { get; set; } = null!;
         public string ArtWorkImage { get; set; } = null!;
 
+        public decimal ArtWorkAmount { get; set; }
+
+        public string Series { get; set; } = null!;
+
         public int UserId { get; set; }
     }
 }
